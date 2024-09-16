2051a226f598badc7248fb63edefbd494e906d6c
kartavya <joshikartavya78@gmail.com>
2024-09-16 11:45:32 +0530

    review res.

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -3,56 +3,56 @@ import "./Reviews.scss"
 
 const Reviews = () => {
   return (
-    <div className="review w-screen h-max px-12 py-10 flex flex-col">
+    <div className="review w-screen h-max px-8 py-6 lg:px-12 lg:py-10 flex flex-col">
       <div className="title flex flex-row justify-center items-center">
-        <hr className="w-[70%] h-4 text-[#222222]" />
-        <h1 className="text-[--secondary] overflow-y-hidden">our reviews <i class="ri-gemini-fill"></i></h1>
+        <hr className="lg:w-[70%] lg:h-4 text-[#222222]" />
+        <h1 className="text-4xl md:text-6xl lg:text-6xl text-[--secondary] overflow-y-hidden">our reviews <i className="ri-gemini-fill"></i></h1>
       </div>
 
       <div className="testimonials flex flex-col gap-12">
-        <div className="review1 mt-12 flex flex-row justify-between items-center w-full h-[80%]">
-          <div className="text w-1/2  h-full">
-            <h1 className="text-5xl text-[--secondary] font-clash font-medium overflow-y-hidden">"Riviera retreat: exquisite luxury, imapable services. a heaven of tranquility. the perfact escape for discerning travellers seeking opluence and relextaion"</h1>
+        <div className="review1 mt-6 md:mt-9 lg:mt-12 flex flex-col-reverse lg:flex-row justify-between items-center lg:w-full lg:h-[80%]">
+          <div className="text lg:w-1/2  lg:h-full">
+            <h1 className=" text-lg md:text-3xl lg:text-5xl text-[--secondary] font-clash font-medium overflow-y-hidden">"Riviera retreat: exquisite luxury, imapable services. a heaven of tranquility. the perfact escape for discerning travellers seeking opluence and relextaion"</h1>
             </div>
-          <div className="img-container w-1/2 h-full flex items-center justify-center">
+          <div className="img-container lg:w-1/2 h-full flex items-center justify-center">
             <div className="img-1 drop-shadow-2xl">
-              <div className="frame bg-[#e1e1e1] h-[22rem] w-[20rem] flex flex-col items-center">
-                <div className="frame-img relative mt-10 h-60 w-60 bg-blue-500">
+              <div className="frame bg-[#e1e1e1] lg:h-[22rem] lg:w-[20rem] flex flex-col items-center">
+                <div className="frame-img relative lg:mt-10 h-40 w-40 md:h-60 md:w-60 lg:h-60 lg:w-60">
                   <Image src="/user2.jpeg" fill />
                 </div>
-                <h1 className="text-3xl text-[--secondary] font-clash font-semibold mt-8">John doe</h1>
+                <h1 className="text-lg md:text-2xl lg:text-3xl text-[--secondary] font-clash font-semibold mt-8">John doe</h1>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="review2 mt-12 flex flex-row-reverse justify-between items-center w-full h-[65%]">
-          <div className="text w-1/2  h-full">
-            <h1 className="text-5xl text-[--secondary] font-clash font-medium text-right overflow-y-hidden">"Riviera retreat: exquisite luxury, imapable services. a heaven of tranquility. the perfact escape for discerning travellers seeking opluence and relextaion"</h1>
-          </div>
-          <div className="img-container w-1/2 h-full flex items-center justify-center">
+        <div className="review1 mt-6 md:mt-9 lg:mt-12 flex flex-col-reverse lg:flex-row-reverse justify-between items-center lg:w-full lg:h-[80%]">
+          <div className="text lg:w-1/2  lg:h-full">
+            <h1 className=" text-lg md:text-3xl lg:text-5xl text-[--secondary] font-clash font-medium overflow-y-hidden">"Riviera retreat: exquisite luxury, imapable services. a heaven of tranquility. the perfact escape for discerning travellers seeking opluence and relextaion"</h1>
+            </div>
+          <div className="img-container lg:w-1/2 h-full flex items-center justify-center">
             <div className="img-1 drop-shadow-2xl">
-              <div className="frame bg-[#e1e1e1] h-[22rem] w-[22rem] flex flex-col items-center">
-                <div className="frame-img relative mt-10 h-60 w-60 bg-blue-500">
+              <div className="frame bg-[#e1e1e1] lg:h-[22rem] lg:w-[20rem] flex flex-col items-center">
+                <div className="frame-img relative lg:mt-10 h-40 w-40 md:h-60 md:w-60 lg:h-60 lg:w-60">
                   <Image src="/user3.jpeg" fill />
                 </div>
-                <h1 className="text-3xl text-[--secondary] font-clash font-semibold mt-8">John doe</h1>
+                <h1 className="text-lg md:text-2xl lg:text-3xl text-[--secondary] font-clash font-semibold mt-8">John doe</h1>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="review3 mt-12 flex flex-row justify-between items-center w-full h-[65%]">
-          <div className="text w-1/2  h-full">
-            <h1 className="text-5xl text-[--secondary] font-clash font-medium overflow-y-hidden">"Riviera retreat: exquisite luxury, imapable services. a heaven of tranquility. the perfact escape for discerning travellers seeking opluence and relextaion"</h1>
-          </div>
-          <div className="img-container w-1/2 h-full flex items-center justify-center">
+        <div className="review1 mt-6 md:mt-9 lg:mt-12 flex flex-col-reverse lg:flex-row justify-between items-center lg:w-full lg:h-[80%]">
+          <div className="text lg:w-1/2  lg:h-full">
+            <h1 className=" text-lg md:text-3xl lg:text-5xl text-[--secondary] font-clash font-medium overflow-y-hidden">"Riviera retreat: exquisite luxury, imapable services. a heaven of tranquility. the perfact escape for discerning travellers seeking opluence and relextaion"</h1>
+            </div>
+          <div className="img-container lg:w-1/2 h-full flex items-center justify-center">
             <div className="img-1 drop-shadow-2xl">
-              <div className="frame bg-[#e1e1e1] h-[22rem] w-[22rem] flex flex-col items-center">
-                <div className="frame-img relative mt-10 h-60 w-60 bg-blue-500">
+              <div className="frame bg-[#e1e1e1] lg:h-[22rem] lg:w-[20rem] flex flex-col items-center">
+                <div className="frame-img relative lg:mt-10 h-40 w-40 md:h-60 md:w-60 lg:h-60 lg:w-60">
                   <Image src="/user2.jpeg" fill />
                 </div>
-                <h1 className="text-3xl text-[--secondary] font-clash font-semibold mt-8">John doe</h1>
+                <h1 className="text-lg md:text-2xl lg:text-3xl text-[--secondary] font-clash font-semibold mt-8">John doe</h1>
               </div>
             </div>
           </div>
